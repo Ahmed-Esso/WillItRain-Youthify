2ea7a50183e62ac5016ff4b35d072c0802d0fef4
Ahmed Essam <a7med.esso05@gmail.com>
2025-09-30 22:04:07 +0300

    Update pipeline definition for Dagster
    # ده اللي Dagster هيستخدمه عشان يلاقي الـ jobs
    Updated the pipeline definition to use nasa_climate_pipeline.

--- nasa_snowflake/__init__.py
+++ nasa_snowflake/__init__.py
@@ -1,1 +1,7 @@
-from nasa_snowflake.my_pipeline import nasa_temperature_pipeline
+from dagster import Definitions
+from .my_pipeline import nasa_climate_pipeline
+
+# ده اللي Dagster هيستخدمه عشان يلاقي الـ jobs
+defs = Definitions(
+    jobs=[nasa_climate_pipeline]  # غيرنا الاسم للصحيح
+)
