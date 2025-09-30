275b2309a24bf781a2cb921b335de41bb8f373c5
Ahmed Essam <a7med.esso05@gmail.com>
2025-09-30 20:11:00 +0300

    Integrate Dagster Definitions for pipelines
    Added Dagster Definitions for job management.

--- nasa_snowflake/__init__.py
+++ nasa_snowflake/__init__.py
@@ -1,1 +1,7 @@
+from dagster import Definitions
 from .my_pipeline import nasa_variables_pipeline
+
+# ده اللي Dagster هيستخدمه عشان يلاقي الـ jobs
+defs = Definitions(
+    jobs=[nasa_variables_pipeline]
+)
