6ffc32a186492cbebec4624e53687ce9c8231383
Ahmed Essam <a7med.esso05@gmail.com>
2025-09-30 22:07:25 +0300

    Replace pipeline reference in Definitions

--- nasa_snowflake/__init__.py
+++ nasa_snowflake/__init__.py
@@ -1,7 +1,7 @@
 from dagster import Definitions
-from .my_pipeline import nasa_climate_pipeline
+from .my_pipeline import nasa_variables_pipeline
 
 # ده اللي Dagster هيستخدمه عشان يلاقي الـ jobs
 defs = Definitions(
-    jobs=[nasa_climate_pipeline]  # غيرنا الاسم للصحيح
+    jobs=[nasa_variables_pipeline]
 )
