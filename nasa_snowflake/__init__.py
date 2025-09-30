97b4c5b95eab763dfaa1ac49af2a7aeec148670d
mayar hany rafik <mayarhany1999@gmail.com>
2025-09-30 20:40:44 +0300

    Update __init__.py

--- nasa_snowflake/__init__.py
+++ nasa_snowflake/__init__.py
@@ -1,7 +1,2 @@
-from dagster import Definitions
 from .my_pipeline import nasa_temperature_pipeline
 
-# Dagster هيلاقي الـ job الجديد هنا
-defs = Definitions(
-    jobs=[nasa_temperature_pipeline]
-)
