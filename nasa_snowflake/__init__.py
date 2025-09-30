fb3465698e0a58ca50978af0324fc38e230f7d09
Ahmed Essam <a7med.esso05@gmail.com>
2025-10-01 00:51:27 +0300

    Combine job definitions for weather and wind pipelines

--- nasa_snowflake/__init__.py
+++ nasa_snowflake/__init__.py
@@ -1,11 +1,8 @@
 from dagster import Definitions
 from .nasa_daily_weather_pipeline import nasa_daily_weather_pipeline
 from .nasa_daily_wind_pipeline import nasa_daily_wind_pipeline
+
 # ده اللي Dagster هيستخدمه عشان يلاقي الـ jobs
 defs = Definitions(
-    jobs=[nasa_daily_wind_pipeline]
+    jobs=[nasa_daily_weather_pipeline, nasa_daily_wind_pipeline]
 )
-defs = Definitions(
-    jobs=[nasa_daily_weather_pipeline]
-)
-
