e84eade899069cffbd5a99b604a72610f4403bdc
mayar hany rafik <mayarhany1999@gmail.com>
2025-10-01 17:38:27 +0300

    Update __init__.py

--- nasa_snowflake/__init__.py
+++ nasa_snowflake/__init__.py
@@ -3,11 +3,14 @@ from .nasa_daily_weather_pipeline import nasa_daily_weather_pipeline
 from .nasa_daily_wind_pipeline import nasa_daily_wind_pipeline
 from .nasa_daily_dewpoint_2022_pipeline import nasa_daily_dewpoint_2022_pipeline
 from .nasa_daily_wetbulb_2022_pipeline import nasa_daily_wetbulb_2022_pipeline
+from .nasa_daily_wind_speed_2022_pipeline import nasa_daily_wind_speed_2022_pipeline
+
 
 
 # ده اللي Dagster هيستخدمه عشان يلاقي الـ jobs
 defs = Definitions(
-    jobs=[nasa_daily_weather_pipeline, nasa_daily_wind_pipeline,nasa_daily_dewpoint_2022_pipeline,nasa_daily_wetbulb_2022_pipeline]
+    jobs=[nasa_daily_weather_pipeline, nasa_daily_wind_pipeline,nasa_daily_dewpoint_2022_pipeline,nasa_daily_wetbulb_2022_pipeline,nasa_daily_wind_speed_2022_pipeline]
 )
+
 
 
