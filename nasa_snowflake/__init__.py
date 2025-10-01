2c65ec9e54b56c39ccdc0ad3d890ab802b045634
Ahmed Essam <a7med.esso05@gmail.com>
2025-10-01 23:29:11 +0300

    Update __init__.py

--- nasa_snowflake/__init__.py
+++ nasa_snowflake/__init__.py
@@ -1,18 +1,15 @@
-from dagster import Definitions, job
+from dagster import Definitions, JobDefinition
 import pkgutil, importlib, inspect, sys
 
-# هنجيب اسم الباكيج (مثلاً nasa_snowflake)
 package_name = __name__
 
 jobs = []
 
-# نعمل import لكل modules في الباكيج
 for _, module_name, _ in pkgutil.iter_modules(sys.modules[package_name].__path__):
     module = importlib.import_module(f"{package_name}.{module_name}")
-    # ندور على أي object معمول له decorator @job
     for name, obj in inspect.getmembers(module):
-        if isinstance(obj, job):
+        if isinstance(obj, JobDefinition):  # ✅ بدلاً من job
+            print(f"✅ Loaded job: {name}")
             jobs.append(obj)
 
-# نعرف الـ definitions
 defs = Definitions(jobs=jobs)
