b7230ce9f3eff75953f3531522f41c4c505b2ac1
mayar hany rafik <mayarhany1999@gmail.com>
2025-10-03 12:41:05 +0300

    Update nasa_daily_total_precipitable_water_2022_pipeline.py

--- nasa_snowflake/nasa_daily_total_precipitable_water_2022_pipeline.py
+++ nasa_snowflake/nasa_daily_total_precipitable_water_2022_pipeline.py
@@ -78,7 +78,7 @@ def search_nasa_files_tql_2022(context):
     results = earthaccess.search_data(
         short_name="M2T1NXSLV",
         version="5.12.4",
-        temporal=("2022-01-01", "2022-1-15"),
+        temporal=("2022-01-01", "2022-12-31"),
         bounding_box=(25.0, 22.0, 37.0, 32.0)
     )
     
