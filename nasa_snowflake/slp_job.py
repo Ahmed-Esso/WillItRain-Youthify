3b5f90f56153faa4db2c0e4db3d7f9dffeaabe1f
Ahmed Essam <a7med.esso05@gmail.com>
2025-10-03 16:16:59 +0300

    Update slp_job.py

--- nasa_snowflake/slp_job.py
+++ nasa_snowflake/slp_job.py
@@ -19,7 +19,7 @@ def search_slp_files(context):
     auth = earthaccess.login(strategy="environment")
     results = earthaccess.search_data(
         short_name=DATASET,
-        temporal=("2022-01-01", "2022-01-07"),
+        temporal=("2022-01-01", "2022-12-31"),
         bounding_box=ALEX_BOUNDING_BOX
     )
     for i, granule in enumerate(results):
